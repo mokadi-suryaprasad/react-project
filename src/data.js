a331695f8fcf77177f1b663034741b9da01fb065
Mokadi Surya Prasad <msuryaprasad11@gmail.com>
2025-05-18 11:53:19 +0530

    Updated data.js file

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const sliderItems = [
   {
     id: "1",
     img: "assets/braclet/main.jfif",
-    title: "Helm charts have been successfully configured using ArgoCD",
+    title: "Deployed with build and deploy servers",
     desc: "This is now in new commit",
     bg: "green",
   },
