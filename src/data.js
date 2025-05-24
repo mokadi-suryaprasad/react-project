f89196fe699880c79f9483d47a8d40b0b5317474
Mokadi Surya Prasad <msuryaprasad11@gmail.com>
2025-05-24 13:30:17 +0530

    data.js files modified

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const sliderItems = [
   {
     id: "1",
     img: "assets/braclet/main.jfif",
-    title: "Deployed with helm and docker",
+    title: "Deployed with helm",
     desc: "This is now in new commit",
     bg: "green",
   },
